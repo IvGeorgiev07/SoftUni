fb1fb276c8245b5294741e67d59f0c8995e1dd1c
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-06-14 14:10:21 +0300

    14.06.22

---
+++ arrays/array-exercise/project1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] input = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+                sum += input[i];
+            }
+
+            foreach(int i in input)
+            {
+                Console.Write($"{i} ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ arrays/array-exercise/project3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] temp = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+ 
+                if (i + 1 % 2 == 0)
+                {
+                    arr2[i] = temp[1];
+                }
+                
+            }
+
+            foreach(int i in arr1)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            foreach (int i in arr2)
+            {
+                Console.Write($"{i} ");
+            }
+
+        }
+    }
+}
