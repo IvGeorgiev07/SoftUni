afdb8c4c8b7be23b66c26e74f8c756cfafc150e5
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-06-16 14:15:02 +0300

    16.06.22

---
+++ arrays/array-exercise/project5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> output = new List<int>();
+            int maxValue = int.MinValue;
+
+            findMaxValue(arr1, maxValue, output);
+            output.Reverse();
+
+            foreach (int i in output)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+        static void findMaxValue(int[] arr1, int maxValue, List<int> output)
+        {
+            for (int i = arr1.Length - 1; i >= 0; i--)
+            {
+                if (arr1[i] > maxValue)
+                {
+                    maxValue = arr1[i];
+                    output.Add(maxValue);
+                }
+
+            }
+        }
+    }
+}
---
+++ arrays/array-exercise/project6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace project6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int counter = 0;
+            int mid = 0;
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                equalSum(counter, input, mid, leftSum, rightSum);
+                if (mid < input.Length - 1)
+                {
+                    mid++;
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+
+        static void equalSum(int counter, int[] input, int mid, int leftSum, int rightSum)
+        {
+
+
+            for (int j = 0; j < input[mid]; j++)
+            {
+                leftSum += input[j];
+            }
+
+            for (int j = input.Length - 1; j > input[mid]; j--)
+            {
+                rightSum += input[j];
+            }
+
+            if (leftSum == rightSum)
+            {
+                counter++;
+            }
+            //Console.WriteLine(leftSum);
+            //Console.WriteLine(rightSum);
+
+
+
+
+        }
+    }
+}
