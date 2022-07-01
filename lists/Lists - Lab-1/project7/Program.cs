50bd5362942a384478221781b8ddb84738fe09d9
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-07-01 13:31:38 +0300

    01.07.22
    List lab -1 complete

--- lists/Lists - Lab-1/project7/Program.cs
+++ lists/Lists - Lab-1/project7/Program.cs
@@ -9,69 +9,151 @@ namespace project6
         static void Main(string[] args)
         {
             List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
-
+            int modifier = 0;
+            int modifier2 = 0;
+            List<int> copy = input;
             string[] input2 = Console.ReadLine().Split().ToArray();
+            bool change = false;
 
             while (input2[0] != "end")
             {
-                string current = input2[0]; // for debugging only
-                if (input2[0] == "Contains")
+                if (input2[0] == "Remove")
                 {
-                    checkItemsAreEqual(input, input2);
-                    
+                    inputModifier(input, input2, modifier, modifier2);
+                    change = true;
+                }
+                else if (input2[0] == "Add")
+                {
+                    inputModifier(input, input2, modifier, modifier2);
+                    change = true;
+                }
+                else if (input2[0] == "Insert")
+                {
+                    inputModifier(input, input2, modifier, modifier2);
+                    change = true;
+                }
+                else if (input2[0] == "RemoveAt")
+                {
+                    inputModifier(input, input2, modifier, modifier2);
+                    change = true;
+                }
+                else if (input2[0] == "Contains")
+                {
+                    bool equalNums = false;
+                    checkItemsAreEqual(input, input2, ref equalNums);
+
+                    if (equalNums == true)
+                    {
+                        Console.Write("Yes");
+                        Console.WriteLine();
+                    }
+                    else if (equalNums == false)
+                    {
+                        Console.Write("No such number");
+                        Console.WriteLine();
+                    }
                 }
                 else if (input2[0] == "PrintEven")
                 {
-                    printEvenNums(input);
-                    
+                    int[] evenNums = new int[input.Count];
+                    printEvenNums(input, ref evenNums);
+
+                    for (int i = 0; i < evenNums.Length; i++)
+                    {
+                        if (evenNums[i] != 0)
+                        {
+                            Console.Write($"{evenNums[i]} ");
+                        }
+                    }
+                    Console.WriteLine();
+
                 }
                 else if (input2[0] == "PrintOdd")
                 {
-                    printOddNums(input);
-                    
+                    int[] oddNums = new int[input.Count];
+                    printOddNums(input, ref oddNums);
+
+                    for (int i = 0; i < oddNums.Length; i++)
+                    {
+                        if (oddNums[i] != 0)
+                        {
+                            Console.Write($"{oddNums[i]} ");
+                        }
+                    }
+                    Console.WriteLine();
                 }
                 else if (input2[0] == "GetSum")
                 {
-                    getSum(input);
-                    
+                    int sum = 0;
+                    getSum(input, ref sum);
+
+                    Console.WriteLine(sum);
                 }
                 else if (input2[0] == "Filter")
                 {
-                    filterItems(input, input2);
-                    
+                    List<int> filteredNums = new List<int>();
+                    filterItems(input, input2, ref filteredNums);
+
+                    Console.WriteLine(String.Join(" ", filteredNums));
                 }
 
                 input2 = Console.ReadLine().Split().ToArray();
             }
-            
+            if (change == true)
+            {
+                Console.WriteLine(string.Join(" ", input));
+            }
+
         }
 
-        static void getSum(List<int> input)
+        private static void inputModifier(List<int> input, string[] input2, int modifier, int modifier2)
         {
-            int sum = 0;
-            for(int i = 0; i < input.Count; i++)
+            if (input2[0] == "Add")
+            {
+                modifier = int.Parse(input2[1]);
+                input.Add(modifier);
+            }
+            else if (input2[0] == "Remove")
+            {
+                modifier = int.Parse(input2[1]);
+                input.Remove(modifier);
+            }
+            else if (input2[0] == "RemoveAt")
+            {
+                modifier = int.Parse(input2[1]);
+                input.RemoveAt(modifier);
+            }
+            else if (input2[0] == "Insert")
+            {
+                modifier = int.Parse(input2[1]);
+                modifier2 = int.Parse(input2[2]);
+                input.Insert(modifier2, modifier);
+            }
+        }
+
+        static int getSum(List<int> input, ref int sum)
+        {
+            for (int i = 0; i < input.Count; i++)
             {
                 sum += input[i];
             }
-            Console.Write(sum);
-            Console.WriteLine();
+            return sum;
         }
 
-        static void filterItems(List<int> input, string[] input2)
+        static List<int> filterItems(List<int> input, string[] input2, ref List<int> filteredNums)
         {
             string condition = input2[1];
             int num = int.Parse(input2[2]);
 
-            if(condition == "<")
+            if (condition == "<")
             {
-                for(int i = 0; i < input.Count; i++)
+                for (int i = 0; i < input.Count; i++)
                 {
                     if (input[i] < num)
                     {
-                        Console.Write($"{input[i]} ");
+                        filteredNums.Add(input[i]);
                     }
                 }
-                Console.WriteLine();
             }
             else if (condition == ">")
             {
@@ -79,10 +161,9 @@ namespace project6
                 {
                     if (input[i] > num)
                     {
-                        Console.Write($"{input[i]} ");
+                        filteredNums.Add(input[i]);
                     }
                 }
-                Console.WriteLine();
             }
             else if (condition == ">=")
             {
@@ -90,10 +171,9 @@ namespace project6
                 {
                     if (input[i] >= num)
                     {
-                        Console.Write($"{input[i]} ");
+                        filteredNums.Add(input[i]);
                     }
                 }
-                Console.WriteLine();
             }
             else if (condition == "<=")
             {
@@ -101,61 +181,52 @@ namespace project6
                 {
                     if (input[i] <= num)
                     {
-                        Console.Write($"{input[i]} ");
+                        filteredNums.Add(input[i]);
                     }
                 }
-                Console.WriteLine();
             }
+            return filteredNums;
 
         }
 
-        static void printOddNums(List<int> input)
+        static int[] printOddNums(List<int> input, ref int[] oddNums)
         {
             for (int i = 0; i < input.Count; i++)
             {
                 if (input[i] % 2 != 0)
                 {
-                    Console.Write($"{input[i]} ");
+                    oddNums[i] = input[i];
                 }
             }
-            Console.WriteLine();
+            return oddNums;
         }
 
-        static void printEvenNums(List<int> input)
+        static int[] printEvenNums(List<int> input, ref int[] evenNums)
         {
-            for(int i = 0; i < input.Count; i++)
+            for (int i = 0; i < input.Count; i++)
             {
                 if (input[i] % 2 == 0)
                 {
-                    Console.Write($"{input[i]} ");
+                    evenNums[i] = input[i];
                 }
             }
-            Console.WriteLine();
+            return evenNums;
         }
 
-        static void checkItemsAreEqual(List<int> input, string[] input2)
+        static bool checkItemsAreEqual(List<int> input, string[] input2, ref bool equalNums)
         {
             int num = int.Parse(input2[1]);
-            bool isEqual = false;
+
 
             for (int i = 0; i < input.Count; i++)
-            {                
-                if(num == input[i])
+            {
+                if (num == input[i])
                 {
-                    isEqual = true;
-                }               
+                    equalNums = true;
+                }
             }
+            return equalNums;
 
-            if (isEqual == true)
-            {
-                Console.Write("Yes");
-                Console.WriteLine();
-            }
-            else if(isEqual == false)
-            {
-                Console.Write("No such number");
-                Console.WriteLine();
-            }
         }
     }
 }
