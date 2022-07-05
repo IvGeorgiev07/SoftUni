0fbe9c56478c51f8f5b52b4762c25438e5ea7ecb
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-07-05 13:08:24 +0300

    05.07.22

--- lists/Lists - Exercise/project2/Program.cs
+++ lists/Lists - Exercise/project2/Program.cs
@@ -15,14 +15,21 @@ namespace project1
             {
                 if(input2[0] == "Delete")
                 {
-                    input.RemoveAt(int.Parse(input2[1]));
+                    for(int i = 0; i < input.Count; i++)
+                    {
+                        if(input[i] == input[1])
+                        {
+                            input.Remove(int.Parse(input2[1]));
+                        }
+                    }
                 }
-                else if (input2[0] == "Delete")
+                else if (input2[0] == "Insert")
                 {
-
+                    input.Insert(int.Parse(input2[2]), int.Parse(input2[1]));
                 }
+                input2 = Console.ReadLine().Split().ToList();
             }
-
+            Console.WriteLine(String.Join(" ", input));
         }
     }
 }
---
+++ lists/Lists - Exercise/project3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> input = Console.ReadLine().Split().ToList();
+            List<string> attendanceList = new List<string>();
+            int counter = 1;
+            
+
+            while (true)
+            {  
+
+                if (input[2] == "going!")
+                {
+                    attendanceList.Add(input[0]);
+                }
+                else if(input[2] == "not")
+                {
+                    for(int i = 0; i < attendanceList.Count; i++)
+                    {
+                        if(input[0] != attendanceList[i])
+                        {
+                            Console.WriteLine($"{input[0]} is not in the list!");
+                        }
+                        else if (input[0] == attendanceList[i])
+                        {
+                            attendanceList.Remove(input[0]);
+                        }
+                    }
+                }
+                input = Console.ReadLine().Split().ToList();
+                counter++;
+                if (counter > n)
+                {
+                    break;
+                }
+            }
+            for(int i = 0; i < attendanceList.Count; i++)
+            {
+                Console.WriteLine(attendanceList[i]);
+            }
+        }
+    }
+}
