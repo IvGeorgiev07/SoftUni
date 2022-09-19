e355a23febe634e100fac8ec45ac44b6060aa566
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-09-19 21:08:44 +0300

    19.09.22

---
+++ C# Fundamentals/Basic syntax - exercise/Basic Syntax - Exercise/project4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = input; i <= input2; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
---
+++ C# Fundamentals/Basic syntax - exercise/Basic Syntax - Exercise/project5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string password = string.Empty;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char eachChar = input[i];
+                password += eachChar;
+            }
+            
+
+            string input2 = Console.ReadLine();
+            int counter = 1;
+            while (input2 != password)
+            {
+                if(counter < 4)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else if(counter == 4)
+                {
+                    Console.WriteLine($"User {input} blocked!");
+                    return;
+                }
+               counter++;
+
+               input2 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"User {input} logged in.");
+        }
+    }
+}
