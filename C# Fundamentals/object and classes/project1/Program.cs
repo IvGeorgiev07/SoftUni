b5be681ab552fdc8e4f3767daa302581bf2aa5b3
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-11-08 20:38:00 +0200

    08.11.22

---
+++ C# Fundamentals/object and classes/project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Random rnd = new Random();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                int randomIndex = rnd.Next(input.Length);
+
+                string a = input[randomIndex];
+                string b = input[i];
+
+                input[randomIndex] = b;
+                input[i] = a;
+            }
+            Console.WriteLine(String.Join(Environment.NewLine, input));
+
+
+        }
+    }
+        
+}
---
+++ C# Fundamentals/object and classes/project2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+
+namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger f = 1;
+            for(int i = 2; i <= n; i++)
+            {
+                f *=i;
+            }
+
+            Console.WriteLine(f);
+        }
+        
+    }
+}
