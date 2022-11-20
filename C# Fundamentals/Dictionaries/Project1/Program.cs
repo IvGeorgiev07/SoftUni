7b43d4f2902c5532bfd74d0ccf600928673c304b
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-11-20 11:56:43 +0200

    20.11.22

---
+++ C# Fundamentals/Dictionaries/Project1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Tracing;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input= Console.ReadLine();
+            char[] chars = input.Where(c => !char.IsWhiteSpace(c)).ToArray();
+
+            Dictionary<char, int> words= new Dictionary<char, int>();
+
+            foreach (char c in chars)
+            {
+                if (!words.ContainsKey(c))
+                {
+                    words.Add(c, 0);
+                }
+
+                words[c]++;
+            }
+            
+            foreach(var kvp in words)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+            
+        }
+    }
+}
