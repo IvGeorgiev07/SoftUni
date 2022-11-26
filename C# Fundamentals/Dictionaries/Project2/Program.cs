602c2e93567c93695b3a3ee30c39680ba3762a5f
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2022-11-26 18:06:18 +0200

    26.11.22

---
+++ C# Fundamentals/Dictionaries/Project2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.Tracing;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = Console.ReadLine();
+            int qty = int.Parse(Console.ReadLine());
+            Dictionary<string, int> resouceQty = new Dictionary<string, int>();
+
+            while(resource != "stop")
+            {
+                if (!resouceQty.ContainsKey(resource))
+                {
+                    resouceQty.Add(resource, qty);
+                }
+                else if (resouceQty.ContainsKey(resource))
+                {
+                    resouceQty[resource] += qty;
+                }
+
+                resource = Console.ReadLine();
+                if (resource != "stop")
+                {
+                    qty = int.Parse(Console.ReadLine());
+                }
+                
+            }
+
+            foreach(var kvp in resouceQty)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Dictionaries/Project3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Tracing;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<decimal, int> counts = new Dictionary<decimal, int>();
+            Dictionary<string, Dictionary counts> ProductSum = new Dictionary<string, counts>();
+            
+            while (true)
+            {
+                string[] product = Console.ReadLine().Split();
+                if (product[0] == "buy")
+                {
+                    break;
+                }
+
+                if (!ProductSum.ContainsKey(product[0]))
+                {
+                    decimal price = decimal.Parse(product[1]);
+                    int count = int.Parse(product[2]);
+                    ProductSum.Add(product[0], new Dictionary<decimal, int> { { price, count } });
+                }
+                else
+                {
+                    
+                }
+                
+                
+            }
+
+            foreach (var kvp in ProductSum)
+            {
+                Console.WriteLine($"{kvp.Key} {kvp.Value}");
+            }
+
+        }
+    }
+}
