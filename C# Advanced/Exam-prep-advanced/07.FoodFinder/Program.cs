6c5e7ba578e0a16b4a4d7fc375b855ad87db067f
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2023-02-17 20:45:22 +0200

    17.02.23

---
+++ C# Advanced/Exam-prep-advanced/07.FoodFinder/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+
+namespace _07.BakeryShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputedWater = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            double[] inputedFlour = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Queue<double> water = new Queue<double>(inputedWater);
+            Stack<double> flour = new Stack<double>(inputedFlour);
+
+            Dictionary<string, int> foods = new Dictionary<string, int>
+            {
+                { "Croissant", 0 },
+                { "Muffin", 0 },
+                { "Baguette", 0 },
+                { "Bagel", 0 }
+            };
+            while (water.Any() && flour.Any() )
+            {
+                double currentWater = water.Dequeue();
+                double currentFlour = flour.Pop();
+                double waterPercentage = currentWater*100/(currentWater+currentFlour);
+                
+                if(waterPercentage == 50)
+                {
+                    foods["Croissant"]++;
+                }
+                else if(waterPercentage == 40)
+                {
+                    foods["Muffin"]++;
+                }
+                else if (waterPercentage == 30)
+                {
+                    foods["Baguette"]++;
+                }
+                else if (waterPercentage == 20)
+                {
+                    foods["Bagel"]++;
+                }
+                else if (currentFlour >= currentWater)
+                {
+                    double returningFlour = currentFlour - currentWater;
+                    foods["Croissant"]++;
+                    if(returningFlour > 0)
+                    {
+                        flour.Push(returningFlour);
+                    }
+                    
+                    
+                }
+                //else
+                //{
+                //    flour.Push(currentFlour);
+                //    water.Enqueue(currentWater);
+                //}
+            }
+
+            var sb = new StringBuilder();
+            var ordered = foods.OrderBy(x => x.Key).OrderByDescending(x=>x.Value);
+            if(foods.Any())
+            {
+                foreach (var food in ordered)
+                {
+                    if (food.Value > 0)
+                    {
+                        sb.AppendLine($"{food.Key}: {food.Value}");
+                    }
+
+                }
+
+            }
+            
+            if (water.Count> 0)
+            {
+                sb.AppendLine($"Water left: {string.Join(", ", water)}");
+            }
+            else
+            {
+                sb.AppendLine($"Water left: None");
+            }
+            
+            if(flour.Count> 0)
+            {
+                sb.AppendLine($"Flour left: {string.Join(", ", flour)}");
+            }
+            else
+            {
+                sb.AppendLine($"Flour left: None");
+            }
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
---
+++ C# Advanced/Exam-prep-advanced/FishingNet/FishingNet/Net.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        private readonly ICollection<Fish> fish;
+        public Net(string material, int capacity)
+        {
+            Material = material;
+            Capacity = capacity;
+            this.fish = new List<Fish>();
+        }
+
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+        public IReadOnlyCollection<Fish> Fish => (IReadOnlyCollection<Fish>)this.fish;
+        public int Count { get { return Fish.Count; } }
+
+        public string AddFish(Fish fishToAdd)
+        {
+            if (string.IsNullOrWhiteSpace(fishToAdd.FishType) || fishToAdd.Weight <= 0
+                || fishToAdd.Length <= 0)
+            {
+                return "Invalid fish.";
+            }
+            else if (Fish.Count+1>Capacity)
+            {
+                return "Fishing net is full.";
+            }
+            fish.Add(fishToAdd);
+            return $"Successfully added {fishToAdd.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+           Fish releasedFish = fish.FirstOrDefault(x => x.Weight == weight);
+            if(releasedFish != null)
+            {
+                fish.Remove(releasedFish);
+                return true;
+            }
+            return false;
+        }
+
+        public Fish GetFish(string fishType) 
+            => fish.FirstOrDefault(x=>x.FishType== fishType);
+
+        public Fish GetBiggestFish()
+        {
+            Fish biggestFish = fish.OrderByDescending(x => x.Length).First();
+            return biggestFish;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            List<Fish> sorted = fish.OrderByDescending(x=>x.Length).ToList();
+            sb.AppendLine($"Into the {Material}:");
+            foreach(Fish f in sorted)
+            {
+                sb.AppendLine(f.ToString().Trim());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
