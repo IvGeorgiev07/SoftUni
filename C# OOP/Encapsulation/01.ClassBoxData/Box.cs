d05b37d9f07049981399e2f08dd664f19bcc399f
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2023-03-05 19:20:46 +0200

    05.03.23

---
+++ C# OOP/Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ClassBoxData
+{
+    internal class Box
+    {
+		private double length;
+        private double width;
+        private double height;
+
+        public Box(double length,double width,double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+		public double Length
+        {
+			get => length;
+			set
+			{
+				if(value <=0)
+				{
+					throw new ArgumentException("Length cannot be zero or negative.");
+				}
+				this.length= value;
+			}
+		}
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+           double surfaceArea = 2*length* width + 2* length* height + 2* width * height;
+            return surfaceArea;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2* length* height + 2* width* height;
+            return lateralSurfaceArea;
+        }
+
+        public double Volume()
+        {
+            double volume = length* width* height;
+            return volume;
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/01.ClassBoxData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace _01.ClassBoxData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new(length, width, height);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Surface Area - {box.SurfaceArea():f2}");
+                sb.AppendLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                sb.AppendLine($"Volume - {box.Volume():f2}");
+                Console.WriteLine(sb.ToString().Trim());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
