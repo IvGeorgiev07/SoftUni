deb483820447b17c3565200c9a9c4d7d29f99055
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2023-03-05 20:57:55 +0200

    05.03.23

---
+++ C# OOP/Encapsulation/03.ShoppingSpree/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ShoppingSpree
+{
+    internal class Person
+    {
+        private string name;
+        private decimal money;
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            BagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        private List<Product> BagOfProducts { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            BagOfProducts.Add(product);
+        }
+
+        public string ReadBag()
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> products = new List<string>();
+            if(BagOfProducts.Count > 0)
+            {
+                foreach(Product product in BagOfProducts)
+                {
+                    products.Add(product.Name);
+                }
+                sb.Append(String.Join(", ",products));
+            }
+            else
+            {
+                sb.Append("Nothing bought");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/03.ShoppingSpree/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _03.ShoppingSpree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> person = new Dictionary<string, Person>();
+            Dictionary<string, Product> product = new Dictionary<string, Product>();
+
+            string[] input = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in input)
+            {
+                try
+                {
+                    string[] items = item.Split("=");
+                    Person newPerson = new(items[0], decimal.Parse(items[1]));
+                    person.Add(items[0], newPerson);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+
+            }
+
+            string[] input2 = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in input2)
+            {
+                try
+                {
+                    string[] items = item.Split("=");
+                    Product newProduct = new(items[0], decimal.Parse(items[1]));
+                    product.Add(items[0], newProduct);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+
+            }
+
+
+            string[] input3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input3[0] != "END")
+            {
+                string name = input3[0];
+                string currentProduct = input3[1];
+                if (person[name].Money - product[currentProduct].Cost >= 0)
+                {
+                    person[name].Money -= product[currentProduct].Cost;
+                    person[name].AddProduct(product[currentProduct]);
+                    Console.WriteLine($"{name} bought {currentProduct}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person[name].Name} can't afford {product[currentProduct].Name}");
+                }
+                input3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (person != null)
+            {
+                foreach (var item in person)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value.ReadBag()}");
+                }
+
+            }
+        }
+    }
+}
