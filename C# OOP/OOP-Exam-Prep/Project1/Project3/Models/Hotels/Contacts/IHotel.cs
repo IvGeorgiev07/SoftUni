503962bf87e3cbfbe0a857f38aefc981cc7429a6
IvGeorgiev07 <106436550+IvGeorgiev07@users.noreply.github.com>
2023-04-07 10:37:49 +0300

    07.04.23

---
+++ C# OOP/OOP-Exam-Prep/Project1/Project3/Models/Hotels/Contacts/IHotel.cs
@@ -0,0 +1,16 @@
+﻿namespace BookingApp.Models.Hotels.Contacts
+{
+    using BookingApp.Repositories.Contracts;
+    using global::BookingApp.Models.Bookings.Contracts;
+    using global::BookingApp.Models.Rooms.Contracts;
+    using System.Collections.Generic;
+    public interface IHotel
+    {
+        string FullName { get; }
+        int Category { get; }
+        double Turnover { get; }
+
+        public IRepository<Rooms.Contracts.IRoom> Rooms { get; }
+        public IRepository<Bookings.Contracts.IBooking> Bookings { get; }
+    }
+}
---
+++ C# OOP/OOP-Exam-Prep/Project1/Project3/Models/Hotels/Hotel.cs
@@ -0,0 +1,69 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Hotels
+{
+    internal class Hotel : IHotel
+    {
+        public Hotel(string fullName, int category)
+        {
+            FullName= fullName;
+            Category= category;
+        }
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            private set 
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.HotelNameNullOrEmpty);
+                }
+                fullName = value;
+            }
+        }
+
+        private int category;
+
+        public int Category
+        {
+            get { return category; }
+            private set
+            { 
+                if(value < 1 || value > 5)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCategory);
+                }
+                category = value;
+            }
+        }
+
+        private double turnover;
+
+        public double Turnover => Math.Round(Bookings.All().Sum(x=>x.ResidenceDuration * x.Room.PricePerNight),2);
+
+
+        private IRepository<IRoom> rooms;
+        public IRepository<IRoom> Rooms
+        {
+            get { return rooms; }
+            private set { rooms = value; }
+        }
+
+        private IRepository<IBooking> bookings;
+        public IRepository<IBooking> Bookings
+        {
+            get { return bookings; }
+            private set { bookings = value; }
+        }
+    }
+}
